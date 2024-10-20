be2822a8222e70ec8a0103a85765641c4e1d7888
hirochannel <goldship2486@gmail.com>
2024-10-21 03:27:54 +0900

    maven_installed

---
+++ app_youtubemp3mp4/src/main/java/pro_youtubemp3mp4/App.java
@@ -0,0 +1,13 @@
+package pro_youtubemp3mp4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
